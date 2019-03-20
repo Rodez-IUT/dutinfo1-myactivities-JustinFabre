a81669fef8a5026cbadea4cafbb4c304797fd520
justin.fabre <justin.fabre@iut-rodez.fr>
2019-03-20 14:31:41 +0100

    exo4fini

--- src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
+++ src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
@@ -1,35 +1,54 @@
 CREATE OR REPLACE FUNCTION get_default_owner() RETURNS "user" AS $$
 
-DECLARE 
+  DECLARE 
 
-defaultOwner "user"%rowtype;
-defaultOwnerUsername varchar(500) := "Default Owner";
+    defaultOwner "user"%rowtype;
+    defaultOwnerUsername varchar(500) := "Default Owner";
 
-BEGIN
+  BEGIN
         
-SELECT * into defaultOwner
-FROM "user"
-WHERE username = defaultOwnerUsername;
+    SELECT * into defaultOwner
+    FROM "user"
+    WHERE username = defaultOwnerUsername;
 
 
-IF not found THEN
+      IF not found THEN
 
-INSERT INTO "user"
-VALUES(nextval('id_generator'), defaultOwnerUsername);
+          INSERT INTO "user"
+          VALUES(nextval('id_generator'), defaultOwnerUsername);
 
-SELECT * into defaultOwner
-FROM "user" 
-WHERE username = defaultOwnerUsername;
+          SELECT * into defaultOwner
+          FROM "user" 
+          WHERE username = defaultOwnerUsername;
 
-END IF;
+      END IF;
 
-RETURN defaultOwner ;
+      RETURN defaultOwner ;
 
-END;
+  END
 
 $$ language plpgsql;
 
+CREATE OR REPLACE FUNCTION fix_activities_without_owner() RETURNS SETOF activity AS $$
 
+  DECLARE 
 
+	defaultOwner "user"%rowtype;
+	nowDate date = now();
 
+  BEGIN
 
+	defaultOwner := get_default_owner();
+	return query
+	update activity
+	SET owner_id = defaultOwner.id,
+	    modification_date = nowDate
+	    where owner_id is null
+	    returning *;
+	    
+  END
+ $$ language plpgsql;
+
+	
+	
+	
